fb8683fa7f7a746ef0b639517f7a417c543521e9
Shahin <shahin@Shahins-MacBook-Pro.local>
2023-11-18 23:29:37 +0600

    Added

---
+++ project/save-components/v-show.js
@@ -0,0 +1,27 @@
+const location = "Dhaka"
+const weather = "c"
+
+
+const image = [
+    {
+      src: 'https://dummyimage.com/720x400',
+      title: 'Title 1',
+      alt: 'Title 1',
+      link: 'https://dummyimage.com/720x400'
+    },
+  
+    {
+      src: 'https://dummyimage.com/720x400',
+      title: 'Title 2',
+      alt: 'Title 2',
+      link: 'https://dummyimage.com/720x400'
+    },
+  
+    {
+      src: 'https://dummyimage.com/720x400',
+      title: 'Title 3',
+      alt: 'Title 3',
+      link: 'https://dummyimage.com/720x400'
+    }
+  ]
+export default image;
