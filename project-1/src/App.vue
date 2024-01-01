64550a9f83f2eb0c8076b9f532a7374c442914e1
Shahin <shahin@Shahins-MacBook-Pro.local>
2024-01-01 06:25:56 +0600

    Added new

---
+++ project-1/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+  import {data} from './data/data'
+
+</script>
+
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="container px-6 py-10 mx-auto">
+        <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">{{ data.title }}</h1>
+
+        <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
+            <img :title="data.title" class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96" :src="data.mainImg">
+
+            <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+                <p class="text-sm text-blue-500 uppercase">{{ data.category }}</p>
+
+                <a :href="data.buttonLink" href="#" class="block mt-4 text-2xl font-semibold text-gray-800 hover:underline dark:text-white">
+                    {{ data.subTitle }}
+                </a>
+
+                <p class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
+                    {{ data.content }}
+                </p>
+
+                <a :href="data.buttonLink" class="inline-block mt-2 text-blue-500 underline hover:text-blue-400">{{ data.buttonTxt }}</a>
+
+                <div class="flex items-center mt-6">
+                    <img class="object-cover object-center w-10 h-10 rounded-full" :src="data.thumbNail" alt="">
+
+                    <div class="mx-4">
+                        <h1 class="text-sm text-gray-700 dark:text-gray-200">{{ data.userName }}</h1>
+                        <p class="text-sm text-gray-500 dark:text-gray-400">{{ data.userTitle }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ project-1/src/data/data.js
@@ -0,0 +1,14 @@
+const data = {
+    title: 'From The Blog',
+    mainImg: 'https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    subTitle: 'All the features you want to know',
+    category: 'CATEGORY',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis sint autem nesciunt, laudantium quia tempore delect',
+    buttonTxt: 'Read more',
+    buttonLink: '#',
+    thumbNail: 'https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    userName: 'John Doe',
+    userTitle: 'Developer'
+ }
+
+ export {data}
